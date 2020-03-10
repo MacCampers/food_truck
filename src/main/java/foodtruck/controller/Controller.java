a915dff927ffe23846506f278fc163cc62864363
Benoit <doctor-who48@live.fr>
2020-03-10 12:39:11 +0100

    class

--- src/main/java/foodtruck/controller/Controller.java
+++ src/main/java/foodtruck/controller/Controller.java
@@ -1,4 +1,4 @@
-package com.example.demo.controller;
+package foodtruck.controller;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,50 +15,50 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.demo.model.Actualite;
-import com.example.demo.reposity.ActualiteRepository;
+import foodtruck.model.Produit;
+import foodtruck.reposity.ActualiteRepository;
 
 @RestController
 @RequestMapping("/users")
 public class Controller {
-	@Autowired
-	private ActualiteRepository ar;
-
-	@GetMapping("/info")
-	public String getHello() {
-		return "Greetings from Spring Boot!";
-	}
-
-	@GetMapping("/all")
-	public ArrayList<Actualite> getAll() {
-		ArrayList<Actualite> maList = new ArrayList<Actualite>();
-		Actualite a = new Actualite();
-		a.setDateCreation("10/10");
-		a.setDateFinValidité("10/20");
-		a.setDescription("desc");
-		a.setId(1);
-		a.setImageUrl("image.url");
-		a.setTitle("Title");
-		maList.add(a);
-		maList.add(a);
-		maList.add(a);
-		maList.add(a);
-		return maList;
-	}
-	
-
-	@PostMapping("/create")
-	public void create(@RequestBody Actualite a) {
-		ar.save(a);
-//		Actualite a = new Actualite();
+//	@Autowired
+//	private ActualiteRepository ar;
+//
+//	@GetMapping("/info")
+//	public String getHello() {
+//		return "Greetings from Spring Boot!";
+//	}
+//
+//	@GetMapping("/all")
+//	public ArrayList<Produit> getAll() {
+//		ArrayList<Produit> maList = new ArrayList<Produit>();
+//		Produit a = new Produit();
 //		a.setDateCreation("10/10");
 //		a.setDateFinValidité("10/20");
 //		a.setDescription("desc");
 //		a.setId(1);
 //		a.setImageUrl("image.url");
 //		a.setTitle("Title");
+//		maList.add(a);
+//		maList.add(a);
+//		maList.add(a);
+//		maList.add(a);
+//		return maList;
+//	}
+//	
+//
+//	@PostMapping("/create")
+//	public void create(@RequestBody Produit a) {
 //		ar.save(a);
-		
+////		Actualite a = new Actualite();
+////		a.setDateCreation("10/10");
+////		a.setDateFinValidité("10/20");
+////		a.setDescription("desc");
+////		a.setId(1);
+////		a.setImageUrl("image.url");
+////		a.setTitle("Title");
+////		ar.save(a);
+//		
 	}
 
 
